25c756e3844dd77b940332397e78afdea3f37f36
Michael Russell <mrussell@tombras.com>
2017-02-20 12:30:58 -0500

    default to browser env, and enforce using .js extensions on ESM imports

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ module.exports = {
 	"extends": [
 		"airbnb-base",
 	],
+	"env": {
+		"browser": true
+	},
 	"rules": {
 		"indent": ["error", "tab"],
 		"no-tabs": "off",
-	}
+		"import/extensions": ["error", "always", {
+			"js": "always"
+		}]
+	},
 };
